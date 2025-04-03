744487462282cc008e02d74f76eb5899e66b8b1f
Madiou <124840847+CKILFO@users.noreply.github.com>
2025-04-03 19:05:50 +0200

    Update CKILFO.sol

--- contracts/CKILFO.sol
+++ contracts/CKILFO.sol
@@ -1,26 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract CKILFO is Initializable, ERC20Upgradeable, AccessControlUpgradeable {
+contract CKILFO is Initializable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    uint256 private _cap;
 
     function initialize() public initializer {
         __ERC20_init("CKILFO Token", "CKF");
+        __ERC20Burnable_init();
+        __Pausable_init();
         __AccessControl_init();
+        __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
 
-        uint256 initialSupply = 1_000_000 * 10 ** decimals();
-        _mint(msg.sender, initialSupply);
+        _cap = 1_000_000 * 10 ** decimals(); // Cap total
+        _mint(msg.sender, _cap); // Supply initial = cap
     }
 
-    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) whenNotPaused {
         require(to != address(0), "CKILFO: mint to zero address");
+        require(totalSupply() + amount <= _cap, "CKILFO: cap exceeded");
         _mint(to, amount);
     }
+
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function cap() public view returns (uint256) {
+        return _cap;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
 }
